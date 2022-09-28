ccf8447dd70611ad0f030a07e8c1b522eaf5e236
Krutik Pathak <112356059+KrutikBDA@users.noreply.github.com>
2022-09-27 19:50:56 -0700

    Update CleanTweetsToS3DataFeed.py

--- CleanTweetsToS3DataFeed.py
+++ CleanTweetsToS3DataFeed.py
@@ -14,8 +14,8 @@ import pandas as pd
 
 
 AWS_S3_BUCKET = "prebuiltnodes"
-AWS_ACCESS_KEY_ID = "AKIAQ53INSGAWRSXLBOU"
-AWS_SECRET_ACCESS_KEY = "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"
+AWS_ACCESS_KEY_ID = "*******"
+AWS_SECRET_ACCESS_KEY = "*********"
 #AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
 
 s3_client = boto3.client(
